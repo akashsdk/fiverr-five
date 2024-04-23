278070b61f124e1b4ecc7e96a654cad5fbd95f3e
Tasnim Shahriar Akash <93345259+akashsdk@users.noreply.github.com>
2024-04-24 04:57:32 +0600

    ok

--- src/App.js
+++ src/App.js
@@ -3,15 +3,19 @@ import { Routes, Route, BrowserRouter } from "react-router-dom";
 
 import Home from "./Screen/Home";
 import ErrorPage from "./OtherScreen/ErrorPage";
+import Header from "./Components/Header";
+import Footer from "./Components/Footer";
 
 function App() {
   return (
     <BrowserRouter>
       <div className="App">
+        <Header />
         <Routes>
           <Route path="*" element={<ErrorPage />} />
           <Route path="/" element={<Home />} />
         </Routes>
+        <Footer />
       </div>
     </BrowserRouter>
   );
