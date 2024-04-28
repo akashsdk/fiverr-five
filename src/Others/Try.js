c8ea75dfb982f48abfedf8b414282513afe0304c
Tasnim Shahriar Akash <93345259+akashsdk@users.noreply.github.com>
2024-04-29 02:26:48 +0600

    ok

--- src/Others/Try.js
+++ src/Others/Try.js
@@ -2,6 +2,27 @@ import React from 'react'
 
 export default function Try() {
   return (
-    <div>Try</div>
+    <div>
+
+
+
+
+
+
+
+import Img1 from "../Data/Img1.jpeg";
+import Img2 from "../Data/Img2.jpeg";
+import Img3 from "../Data/Img3.jpeg";
+import Img4 from "../Data/Img4.jpeg";
+import Img5 from "../Data/Img5.jpeg";
+import Img6 from "../Data/Img6.jpeg";
+import Img7 from "../Data/Img7.jpeg";
+import Img8 from "../Data/Img8.jpeg";
+import Img9 from "../Data/Img9.jpeg";
+import Img10 from "../Data/Img10.jpeg";
+
+
+
+    </div>
   )
 }
