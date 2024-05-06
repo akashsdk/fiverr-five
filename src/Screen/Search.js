c696172d7c946faaf474f49dbe240307090bc0cc
Tasnim Shahriar Akash <93345259+akashsdk@users.noreply.github.com>
2024-05-06 22:48:30 +0600

    ko

--- src/Screen/Search.js
+++ src/Screen/Search.js
@@ -4,12 +4,27 @@ import "./Search.css";
 import Header from "../Components/Header";
 import Footer from "../Components/Footer";
 
+import { LeftOutlined } from "@ant-design/icons";
+
 export default function Search() {
+  // Buck-Button
+  const goBack = () => {
+    window.history.back();
+  };
   return (
     <div className="body">
       <Header />
       <div className="box">
-        <div className="Search-Body">okok</div>
+        <div className="Search-Body">
+          <div className="Search-Box">
+            <button onClick={goBack} className="Search-Back-Button">
+              <LeftOutlined className="Search-Back-Icon"/>
+            </button>
+            <div className="Search-Div">
+              <input className="Search-Input-Search"/>
+            </div>
+          </div>
+        </div>
         <Footer />
       </div>
     </div>
