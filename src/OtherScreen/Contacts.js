be41a83d653a3fa16b033d8af7f8d3b6d93d7d46
Tasnim Shahriar Akash <93345259+akashsdk@users.noreply.github.com>
2024-05-12 13:03:35 +0600

    ok

--- src/OtherScreen/Contacts.js
+++ src/OtherScreen/Contacts.js
@@ -1,17 +1,46 @@
-import React from 'react'
-import './Contacts.css';
+import React from "react";
+import "./Contacts.css";
 
 import Header from "../Components/Header";
 import Footer from "../Components/Footer";
 
+import { notification } from 'antd';
+
 export default function Contacts() {
+  const [api, contextHolder] = notification.useNotification();
+  const openNotificationWithIcon = (type) => {
+    api[type]({
+      message: 'Notification Title',
+      description:
+        'This is the content of the notification. This is the content of the notification. This is the content of the notification.',
+    });
+  };
   return (
     <div className="body">
+      {contextHolder}
       <Header />
       <div className="box">
-      Contacts
+        <div className="Contacts-Body">
+          <h1 className="Contacts-Title">Contact Us</h1>
+          <div className="Contacts-Info">
+            <p className="Contacts-Text">Email: cinemaroobd@gmail.com</p>
+            <p className="Contacts-Text">Phone: +1234567890</p>
+            <p className="Contacts-Text">Address: 123 Street, City, Country</p>
+          </div>
+          <div className="Contacts-Form">
+            <h2>Send us a message</h2>
+            <form>
+              <input type="text" placeholder="Name*" />
+              <input type="email" placeholder="Email*" />
+              <input type="phone" placeholder="Phone" />
+              <textarea placeholder="Message*"></textarea>
+              <input type="text" placeholder="Other's" />
+              <button type="submit" onClick={() => openNotificationWithIcon('success')}>Send</button>
+            </form>
+          </div>
+        </div>
         <Footer />
       </div>
     </div>
-  )
+  );
 }
