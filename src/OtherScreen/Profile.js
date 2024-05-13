992911997a19e53a9a23a30cbe2f26166406f47c
Tasnim Shahriar Akash <93345259+akashsdk@users.noreply.github.com>
2024-05-13 14:10:27 +0600

    ok

--- src/OtherScreen/Profile.js
+++ src/OtherScreen/Profile.js
@@ -4,6 +4,11 @@ import "./Profile.css";
 import Header from "../Components/Header";
 import Footer from "../Components/Footer";
 
+import Img1 from "../Img/profile-icon.jpeg";
+
+import { Link } from "react-router-dom";
+import { MessageOutlined, SettingOutlined } from "@ant-design/icons";
+
 export default function Profile() {
   return (
     <div className="body">
@@ -13,7 +18,27 @@ export default function Profile() {
           <div className="Profile-Top-Box">
             <div className="Profile-Box">
               <div className="Profile-Div">
-                ak
+                <img src={Img1} className="Profile-Img1" alt="pic" />
+              </div>
+              <div className="Profile-Div2">
+                <div className="Profile-Div-Top">
+                  <Link className="Profile-Div-Link" to="/settings">
+                    <SettingOutlined />
+                    Settings
+                  </Link>
+
+                  <Link className="Profile-Div-Link" to="/contacts">
+                    <MessageOutlined />
+                    Message
+                  </Link>
+                </div>
+
+                <div className="Profile-Div-Bottom">
+                  <p>Your Good Name</p>
+                  <p>Your Address</p>
+                  <p>Your Email Address</p>
+                  <p>Your Phone Number</p>
+                </div>
               </div>
             </div>
           </div>
